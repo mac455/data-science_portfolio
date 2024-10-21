8fc1fb936287d6646f5fe5f48230ba8085b3e194
mac455 <136570054+mac455@users.noreply.github.com>
2024-10-21 13:50:03 +0100

    Added contact form

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,8 +1,14 @@
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
+import validator from 'validator'; 
 
 export async function POST(req: Request) {
-    const { name, email, message } = await req.json();
+    const { name, email, message, phone } = await req.json();
+
+    // Validate email
+    if (!validator.isEmail(email)) {
+        return NextResponse.json({ error: 'Invalid email address.' }, { status: 400 });
+    }
 
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
@@ -18,9 +24,14 @@ export async function POST(req: Request) {
         await transporter.sendMail({
             from: email,
             to: process.env.RECEIVER_EMAIL,
-            subject: `Contact Form Submission from ${name}`,
+            subject: `${name} wants to connect!`,
             text: message,
-            html: `<p>You have a new message from <strong>${name}</strong> (${email}):</p><p>${message}</p>`,
+            html: `
+                <p><strong>${name}</strong> has submitted a contact form on your website.</p>
+                <p>Their email is: ${email}</p>
+                <p>Their number is: ${phone}</p>
+                <p>Their message is: '${message}'</p>
+            `,
         });
 
         return NextResponse.json({ message: 'Message sent successfully!' }, { status: 200 });
